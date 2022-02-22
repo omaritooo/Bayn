a52c9d9bfe4b6ddc05be4939d8c0b598d8a8f272
omaritooo <themedic227@gmail.com>
2022-02-22 20:21:06 +0200

    trident

---
+++ components/Busine/Head.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="">
+    <div class="flex justify-between gap-x-[183px] px-80">
+      <div class="relative my-auto">
+        <h1
+          class="
+            text-[80px]
+            leading-[100px]
+            font-[500]
+            text-[#2C3A4B]
+            max-w-[542px]
+          "
+        >
+          Sell more with Confidence
+        </h1>
+        <p
+          class="
+            text-[18px]
+            leading-[30px]
+            font-[500]
+            my-5
+            max-h-[151.89px]
+            text-[#394452]
+            max-w-[342.38px]
+          "
+        >
+          Bayn provides one-stop-shop to manage your business and scale up you
+          sales
+        </p>
+        <div class="flex gap-x-[12px]">
+          <button
+            class="
+              bg-black
+              font-[500]
+              leading-[18.78px]
+              rounded-[8px]
+              text-[16px]
+              px-[20px]
+              py-[15px]
+              text-white
+              w-fit
+            "
+          >
+            Start a campaign
+          </button>
+        </div>
+        <svg
+          class="absolute bottom-0 left-1/2"
+          width="259"
+          height="64"
+          viewBox="0 0 259 64"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M2.03906 37.7322C12.7264 49.6415 41.3123 63.4221 67.0353 61.5817C105.839 58.8053 139.906 35.8774 141.7 22.9324C143.495 9.98737 128.305 5.18485 124.298 15.5648C119.365 28.3431 145.51 49.8837 200.018 31.9941C200.018 31.9941 242.542 15.1454 251.422 6.67517"
+            stroke="#1E1E27"
+            stroke-width="3"
+            stroke-linecap="round"
+          />
+          <path
+            d="M240.469 25.0573C244.183 18.9791 252.589 6.37722 256.501 4.59537"
+            stroke="#1E1E27"
+            stroke-width="3"
+            stroke-linecap="round"
+          />
+          <path
+            d="M256.555 4.51582C247.42 6.3454 240.713 6.82267 231.298 2.36805"
+            stroke="#1E1E27"
+            stroke-width="3"
+            stroke-linecap="round"
+          />
+        </svg>
+      </div>
+      <div class="relative">
+        <img src="/Bayn Website (1)/1922 3.png" alt />
+        <div class="absolute flex top-[30%] -left-20">
+          <img src="/Bayn Website (1)/1923.png" alt="" />
+          <img src="/Bayn Website (1)/1924.png" alt="" />
+        </div>
+        <img
+          class="absolute top-[46%] scale-x-[102%] -left-[4.6rem]"
+          src="/Bayn Website (1)/chart.png"
+          alt=""
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
